7b6a9f0c784cb816e62924098406fb3b4a6c7907
zzy <juhnny1125@163.com>
2020-03-30 18:26:54 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>
+        @yield('title', 'Weibo App') - Laravel 新手教程
+    </title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+@section('title','帮助')
+
+@section('content')
+    <h1>
+        帮助页
+    </h1>
+@stop
+
+
