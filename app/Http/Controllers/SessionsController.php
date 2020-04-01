656509db7836d1ca61acbd2b3404b96b60de2072
zzy <juhnny1125@163.com>
2020-04-01 16:20:32 +0800

    '登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,10 +19,11 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials,$request->has('remember'))) {
             session()->flash('success','欢迎回来');
             return redirect()->route('users.show',[Auth::user()]);
         }else {
+            session()->flash('danger','很抱歉,您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
     }
