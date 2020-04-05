80e2a713e5b50e1ef04c9867af5d099bc7751952
zzy <juhnny1125@163.com>
2020-04-05 20:48:54 +0800

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
+//微博列表
+Route::resource('statuses','StatusesController',['only'=>['store','destroy']]);
+
